4a75dd92ea6cf0c4870e683683c4bca7ae03e1f4
TayamaHaruna <smilebabyee@gmail.com>
2025-02-23 08:35:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def handle_message(event):
     elif "記録一覧" in user_message:
         try:
         records = sheet.get_all_values()  # スプレッドシートからデータ取得
-except Exception as e:
+        except Exception as e:
         print(f"エラー: {str(e)}")  # エラー内容をログに出力
         records = []  # エラー時は空リストを代入
 
